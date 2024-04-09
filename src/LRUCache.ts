3d58f7fe917288919fc1dc25e8c610166deb2910
hhk-png <fangluoabc@163.com>
2024-04-09 16:28:13 +0800

    feat: add double link node

--- src/LRUCache.ts
+++ src/LRUCache.ts
@@ -1,5 +1,16 @@
 
-console.log('maxSizeArray.ts')
+class DLinkedNode<K, V> {
+  key: K
+  value: V
+  prev: DLinkedNode<K, V> | null
+  next: DLinkedNode<K, V> | null
+  constructor(key: K, value: V) {
+    this.key = null;
+    this.value = null;
+    this.prev = null
+    this.next = null;
+  }
+}
 
 export class maxSizeArray<T> {
   maxSize: number
